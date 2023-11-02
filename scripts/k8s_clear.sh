559e4d754b449eae3f79e694763bc1356b135c55
msiedlanowski <michal.siedlanowski@ovoo.pl>
2023-11-02 16:09:18 +0100

    preprod config

---
+++ scripts/k8s_clear.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+env=''
+
+print_usage() {
+  printf "Usage: '-r' - prod, '-p' - pre_prod, '-d' - dev"
+}
+
+
+while getopts 'dpr' flag; do
+  case "${flag}" in
+    d) env='dev' ;;
+    p) env='pre_prod' ;;
+    r) env='prod' ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+# Delete all resources
+kubectl delete -k deployments/k8s/overlays/$env
+
+
+
+
+
---
+++ scripts/k8s_deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+env=''
+ns=''
+
+print_usage() {
+  printf "Usage: '-r' - prod, '-p' - pre_prod, '-d' - dev"
+}
+
+while getopts 'dpr' flag; do
+  case "${flag}" in
+    d) env='dev' && ns='nginx-proxy' ;;
+    p) env='pre_prod' && ns='nginx-router-preprod' ;;
+    r) env='prod' && ns='' ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+# Create all resources
+kubectl apply -k deployments/k8s/overlays/$env
+
+sleep 15
+for file in ./assets/images/*; do
+  kubectl cp $file $ns/$(kubectl get pod -n $ns | grep nginx-router | awk '{print $1}'):/assets/$(basename "$file")
+done
+
+# Delete old pod
+kubectl get pod -n $ns | grep nginx-router | awk '{print $1}' | xargs kubectl delete pod -n $ns
