adda61086f12457f32add929e5f9b6326d24aca0
Antoni Sarnowski-Trypka <antoni.sarnowski.trypka@ovoo.pl>
2025-03-24 08:35:30 +0100

    studio installed & post created

---
+++ ec-main-page/pages/blog/[post].vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<template>
+    <div    >
+
+    </div>
+    Hello
+</template>
--- ec-main-page/pages/index.vue
+++ ec-main-page/pages/index.vue
@@ -2,8 +2,7 @@
 
 
   const currentIndex = ref(0);
-  const showSend = ref(true);
-  const checkbox = ref(false);
+
 
 
   const breakingNews = [
@@ -17,46 +16,6 @@
   }
 ];
 
-
-  const contactFormData = ref({
-    email: '',
-    title: '',
-    phone: '',
-    body: ''
-  }) 
-
-const submit = () => {
-    // Make a POST request to the specified URL
-    fetch('https://platformadlaenergii.pl/ec/api/usermgmt/members/contactForm', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(contactFormData.value)
-    })
-            .then(response => {
-              if (response.ok) {
-                // Handle success
-                console.log('Form submitted successfully');
-                // You can add further actions here upon successful submission
-              } else {
-                // Handle errors
-                console.log('Form submission failed');
-              }
-            })
-            .catch(error => {
-              console.error('Error:', error);
-            })
-            .finally(() => {
-              contactFormData.value = {
-                email: '',
-                title: '',
-                phone: '',
-                body: ''
-            }
-              showSend.value=false;
-            });
-  }
 
 
 </script>
@@ -76,15 +35,15 @@ const submit = () => {
   <div id="menu" class="w-full flex justify-between items-center bg-[#FAF3EC] p-3 pt-5 sm:px-10 xl:px-15">
     <img src="/images/logo.svg"  alt="logo" decoding="async" height="90" class="h-[55px] xl:h-[75px]">
     <div id="menuButtons" class="flex flex-1 gap-3 justify-end items-center">
-      <button class="hidden md:flex justify-center items-center gap-2 px-4 py-2 rounded-3xl min-w-[150px] h-[48px] font-medium border-1 text-base"  @click="navigateTo('#articles')">
+      <button class="hidden md:flex justify-center items-center gap-2 px-4 py-2 rounded-3xl min-w-[150px] h-[48px] font-medium border-1 text-base cursor-pointer"  @click="navigateTo('/blog')">
         <span class="">Aktualności</span>
       </button>
-      <button class="hidden sm:flex items-center gap-2 px-4 py-2 rounded-3xl min-w-[150px] h-[48px] font-medium border-1 text-base"  @click="navigateTo('#finish')">
+      <button class="hidden sm:flex items-center gap-2 px-4 py-2 rounded-3xl min-w-[150px] h-[48px] font-medium border-1 text-base cursor-pointer"  @click="navigateTo('#finish')">
         <span >Zamów demo</span>
         <img src="/images/gift.svg" class="h-[25px]" alt="gift icon" decoding="async" height="14">
       </button>
       <img src="/images/divider.svg" class="hidden sm:block h-[37px]" alt="divider" decoding="async" height="37">
-      <button class="px-4 py-2 bg-black rounded-3xl text-white w-[150px] h-[48px] font-medium text-base" @click="navigateTo('https://platformadlaenergii.pl/ec/login', {
+      <button class="px-4 py-2 bg-black rounded-3xl text-white w-[150px] h-[48px] font-medium text-base ursor-pointer" @click="navigateTo('https://platformadlaenergii.pl/ec/login', {
   external: true
 })">Zaloguj</button>
 
@@ -360,56 +319,7 @@ const submit = () => {
   </div>
 </div>
 
-<footer class="bg-[#0D2170] flex flex-col gap-9 sm:gap-15 py-10 px-5 text-white sm:p-15 xl:p-20">
-  <div class="w-full flex flex-col gap-9 sm:gap-15">
-    <div class="w-full flex flex-col gap-5 items-center">
-      <h2 class="text-3xl sm:text-4xl font-medium text-center leading-10">Zostaw nam swoje dane, a skontaktujemy sie z Tobą</h2>
-      <span class="text-base">Odbiór prezentu jest zgodny z <span class="text-[#A2C6FF]">Regulaminem</span></span>
-    </div>
-
-    <form id="contactForm" @submit.prevent="submit" class="text-base flex flex-col lg:flex-row justify-between gap-8 lg:gap-30">
-      <div class="flex flex-col gap-8 flex-1">
-        <div class="formRow">
-          <label for="title">Imię i nazwisko lub nazwa<span class="text-red-600">*</span></label>
-          <input type="text" id="title" name="title" placeholder="Wpisz..." required v-model="contactFormData.title" class="h-[40px] mt-3 w-full border-b border-[#A2C6FF]">
-        </div>
-        <div class="formRow">
-          <label for="phone">Numer telefonu<span class="text-red-600">*</span></label>
-          <input type="tel" id="phone" name="phone" placeholder="Wpisz..." required v-model="contactFormData.phone" class="h-[40px] mt-3 w-full border-b border-[#A2C6FF]">
-        </div>
-        <div class="formRow">
-          <label for="email">Email<span class="text-red-600">*</span></label>
-          <input type="email" id="email" name="email" placeholder="Wpisz..." required v-model="contactFormData.email" class="h-[40px] mt-3 w-full border-b border-[#A2C6FF]">
-        </div>
-      </div>
-      <div class="flex flex-col gap-8 flex-1">
-        <div class="formRow area">
-          <label for="body">Wiadomość</label>
-          <textarea id="body" name="body" rows="4" v-model="contactFormData.body" class="h-[80px] mt-3 w-full border-b border-[#A2C6FF]"></textarea>
-        </div>
-        <span id="terms" class="flex gap-2 items-center">
-            <input type="checkbox" id="ch" name="terms" required v-model="checkbox" class="w-[22px] h-[22px]">
-            <label for="ch">Wypełniając formularz zgadzasz się na <b>Politykę Prywatności</b></label>
-        </span>
-        <div id="sender" class="flex flex-col gap-3 justify-between">
-          <button class="w-full max-w-[400px] bg-white font-medium rounded-3xl text-black py-3" type=submit id="sendBtn" v-if="showSend" :disabled="!checkbox">
-            Wyślij
-          </button>
-          <div id="contactSuccess" v-else>
-            Formularz wysłano poprawnie!
-          </div>
-          <p>Możesz również skontaktowac się z nami pod numerem <span class="font-medium">+48 668 420 820</span></p>
-        </div>
-      </div>
-
-
-    </form>
-  </div>
-  <div id="finish">
-    <img src="/images/logoWhite.svg" class="h-[50px]" alt="logo" decoding="async" height="50">
-    <p class="text-sm mt-3">© Platforma dla energii 2025. All rights reserved.</p>
-  </div>
-</footer>
+<Footer />
 
 
 </template>
